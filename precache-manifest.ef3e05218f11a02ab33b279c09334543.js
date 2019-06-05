501dc01120e5313d732f11a62f4d9fb9482486ac
Robert J. Paruch <roobbb@live.com>
2019-06-04 19:36:31 -0700

    Updates

---
+++ precache-manifest.ef3e05218f11a02ab33b279c09334543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f319a195dda8da8fd0ab5d44d21443a",
+    "url": "/click-just-once/index.html"
+  },
+  {
+    "revision": "f5e8fa9eb970e8c741a0",
+    "url": "/click-just-once/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/click-just-once/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f5e8fa9eb970e8c741a0",
+    "url": "/click-just-once/static/js/main.8afd6ad4.chunk.js"
+  },
+  {
+    "revision": "c7f19ea1af1c59e5c524",
+    "url": "/click-just-once/static/js/runtime~main.c0bf5d4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/click-just-once/static/media/logo.5d5d9eef.svg"
+  }
+]);
